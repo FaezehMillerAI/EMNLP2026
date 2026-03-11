1d25ef08e97907cb17897c607c6c017dae77afc8
FaezehMillerAI <f.safari@exeter.ac.uk>
2026-03-11 13:29:11 +0000

    Update vocab_io.py

--- src/utils/vocab_io.py
+++ src/utils/vocab_io.py
@@ -1,11 +1,17 @@
 import json
-from src.utils.tokenizer import Vocab
+from transformers import GPT2TokenizerFast
+from src.utils.tokenizer import Vocab, HFTokenizerWrapper
 
 
 def load_vocab(path):
     data = json.load(open(path))
+    if data.get("type") == "gpt2":
+        tok = GPT2TokenizerFast.from_pretrained(data.get("name", "gpt2"))
+        if tok.pad_token is None:
+            tok.pad_token = tok.eos_token
+        return HFTokenizerWrapper(tok, max_len=data.get("max_len", 300))
+
     v = Vocab(min_freq=1)
     v.itos = data["itos"]
     v.stoi = {t: i for i, t in enumerate(v.itos)}
     return v
-
